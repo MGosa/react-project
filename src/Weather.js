2d4e90847ab7d2cee04380a0537b966d77e02081
MGosa <149839103+MGosa@users.noreply.github.com>
2024-02-06 22:47:47 +0000

    added Weather css

--- src/Weather.js
+++ src/Weather.js
@@ -14,10 +14,11 @@ export default function Weather(){
                type="search"
                placeholder="Enter a city..."
                className="form-control"
+               autoFocus="on"
              />
            </div>
            <div className="col-3">
-             <input type="submit" value="Search" className="btn btn-primary" />
+             <input type="submit" value="Search" className="btn btn-primary w-100" />
            </div>
          </div>
        </form>
@@ -33,7 +34,8 @@ export default function Weather(){
              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
              alt="Light rain"
            />
-           11°C
+           <span className="temperature">11</span>
+           <span className="unit">°C</span>
          </div>
          <div className="col-6">
            <ul>
