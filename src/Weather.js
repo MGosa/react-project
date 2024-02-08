9abee863116a4929d7cc638278b171573768a9ed
MGosa <149839103+MGosa@users.noreply.github.com>
2024-02-08 00:34:16 +0000

    added search engine functionality

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weather, setWeather] = useState({ ready: false });
+  const [city, setCity]=useState(props.defaultCity);
 
-  function handleResponse(response) {
-    
+    function handleResponse(response){
     setWeather({
       ready:true,
       temperature: response.data.main.temp,
@@ -18,65 +18,53 @@ export default function Weather() {
       wind: response.data.wind.speed,
       city: response.data.name,
     });
+    }
+ 
+  
+
+  function handleSubmit(event){
+    event.preventDefault();
+     search();
   }
 
-  if (weather.ready) {
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                className="form-control"
-                autoFocus="on"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-
-        <h1>{weather.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weather.date} />
-            </li>
-          <li className="text-capitalize">{weather.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img src={weather.iconUrl} alt={weather.description} />
-            <span className="temperature">
-              {Math.round(weather.temperature)}
-            </span>
-            <span className="unit">°C</span>
-          </div>
-
-
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 90% </li>
-              <li>Humidity: {weather.humidity}%</li>
-              <li>Wind: {weather.wind} mph</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-    
-  } else {
-    const apiKey = "094780c710fa4efd669f0df8c3991927";
-    let city = "Glasgow";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return "Loading...";
+  function handleCityChange(event){
+    setCity(event.target.value);
+   
   }
-}
+   function search() {
+     const apiKey = "094780c710fa4efd669f0df8c3991927";
+     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(handleResponse);
+   }
+
+     if (weather.ready) {
+       return (
+         <div className="Weather">
+           <form onSubmit={handleSubmit}>
+             <div className="row">
+               <div className="col-9">
+                 <input
+                   type="search"
+                   placeholder="Enter a city..."
+                   className="form-control"
+                   autoFocus="on"
+                   onChange={handleCityChange}
+                 />
+               </div>
+               <div className="col-3">
+                 <input
+                   type="submit"
+                   value="Search"
+                   className="btn btn-primary w-100"
+                 />
+               </div>
+             </div>
+           </form>
+           <WeatherInfo data={weather} />
+         </div>
+       );
+     } else {
+       search();
+       return "Loading...";
+     }
+   }
