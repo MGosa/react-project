aa429c5ceacab601d8453c4dde1c982d4a314f27
MGosa <149839103+MGosa@users.noreply.github.com>
2024-02-10 22:29:33 +0000

    updated weather icon/added °C/°F conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,28 +1,32 @@
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props){
-    return(
-        <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
+    return (
+      <div className="WeatherInfo">
+        <h1>{props.data.city}</h1>
         <ul>
           <li>
             <FormattedDate date={props.data.date} />
-            </li>
+          </li>
           <li className="text-capitalize">{props.data.description}</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img src={props.data.iconUrl} alt={props.data.description} />
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
+            <div className="d-flex">
+              <div>
+                <img src={props.data.iconUrl} alt={props.data.description} />
+              </div>
+              <div>
+                <WeatherTemperature celsius={props.data.temperature} />
+                
+              </div>
+            </div>
           </div>
-
 
           <div className="col-6">
             <ul>
-              <li>Precipitation: 90% </li>
+              
               <li>Humidity: {props.data.humidity}%</li>
               <li>Wind: {props.data.wind} mph</li>
             </ul>
